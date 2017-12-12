dfa91950ae12aa399bda8f26c379e11ba9945c47
antdevelopment <32327826+antdevelopment@users.noreply.github.com>
2017-12-12 01:43:38 -0800

    revise structure

--- bootcamp/assignments/unit1/lesson2/select_from_where/1stquery.sql
+++
@@ -1,9 +0,0 @@
--- The ID's and durations for all trips of duration greater than 500, ordered by duration.
-SELECT
-    trip_id,
-    duration
-FROM
-    trips
-WHERE
-    duration > 500
-ORDER BY duration ASC
